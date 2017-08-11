e09f7a7574207eacc456e46846263c576a72667c
Justin Oakes <admin@oklasoftware.com>
2017-08-11 11:50:51 -0500

    Initial Commit

---
+++ Readr/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Readr
+//
+//  Created by Justin Oakes on 8/11/17.
+//  Copyright © 2017 Oklasoft LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
