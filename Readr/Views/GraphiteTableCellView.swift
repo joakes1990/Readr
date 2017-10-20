1116957ced8fa46cbd56fa823f785256abad1204
Justin Oakes <justin@justinoak.es>
2017-10-20 04:01:15 -0500

    pushing to GitHub

---
+++ Readr/Views/GraphiteTableCellView.swift
@@ -0,0 +1,19 @@
+//
+//  GraphiteTableCellView.swift
+//  Readr
+//
+//  Created by Justin Oakes on 20/10/17.
+//  Copyright © 2017 Oklasoft LLC. All rights reserved.
+//
+
+import Cocoa
+
+class GraphiteTableCellView: NSTableCellView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
